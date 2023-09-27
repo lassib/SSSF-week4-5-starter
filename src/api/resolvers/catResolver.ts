428d460266e3130efadca372bc054cdfc0da3e7d
lassib <91030337+lassib@users.noreply.github.com>
2023-09-27 20:51:05 +0300

    half pass

--- src/api/resolvers/catResolver.ts
+++ src/api/resolvers/catResolver.ts
@@ -9,3 +9,78 @@ import {Types} from 'mongoose';
 // TODO: create resolvers based on cat.graphql
 // note: when updating or deleting a cat, you need to check if the user is the owner of the cat
 // note2: when updating or deleting a cat as admin, you need to check if the user is an admin by checking the role from the user object
+const catResolver = {
+  Query: {
+    cats: async () => {
+      return await catModel.find();
+    },
+    catById: async (_: unknown, cat: Cat) => {
+      return await catModel.findById(cat.id);
+    },
+    catsByOwner: async (_: unknown, userWithToken: UserIdWithToken) => {
+      return await catModel.find({owner: userWithToken.id});
+    },
+    catsByArea: async (_: unknown, locationInput: locationInput) => {
+      const bounds = rectangleBounds(
+        locationInput.topRight,
+        locationInput.bottomLeft
+      );
+      return await catModel.find({location: {$geoWithin: {$box: bounds}}});
+    },
+  },
+  Mutation: {
+    createCat: async (_: unknown, cat: Cat, user: UserIdWithToken) => {
+      if (!user.token) null;
+      cat.owner = user.id as unknown as Types.ObjectId;
+      const newCat = new catModel({
+        cat_name: cat.cat_name,
+        weight: cat.weight,
+        birthdate: cat.birthdate,
+        filename: cat.filename,
+        location: cat.location,
+        owner: cat.owner,
+      }) as Cat;
+      const createCat = (await catModel.create(newCat)) as Cat;
+      if (!createCat) {
+        throw new GraphQLError('Cat not created');
+      }
+      return createCat;
+    },
+    updateCat: async (_: unknown, cat: Cat, user: UserIdWithToken) => {
+      const catToUpdate = (await catModel.findById(cat.id)) as Cat;
+      if (!user.token || catToUpdate.owner.toString() !== user.id) {
+        throw new GraphQLError('Not authorized');
+      }
+      const updatedCat: Cat = (await catModel.findByIdAndUpdate(cat.id, cat, {
+        new: true,
+      })) as Cat;
+      return updatedCat;
+    },
+    deleteCat: async (_: unknown, cat: Cat, user: UserIdWithToken) => {
+      const catToDelete: Cat = (await catModel.findById(cat.id)) as Cat;
+      if (!user.token || catToDelete.owner.toString() !== user.id) {
+        throw new GraphQLError('Not authorized');
+      }
+      const deletedCat: Cat = (await catModel.findByIdAndDelete(cat.id)) as Cat;
+      return deletedCat;
+    },
+    updateCatAsAdmin: async (_: unknown, cat: Cat, user: UserIdWithToken) => {
+      if (!user.token || user.role !== 'admin') {
+        throw new GraphQLError('Not authorized');
+      }
+      const updateCat: Cat = (await catModel.findByIdAndUpdate(cat.id, cat, {
+        new: true,
+      })) as Cat;
+      return updateCat;
+    },
+    deleteCatAsAdmin: async (_: unknown, cat: Cat, user: UserIdWithToken) => {
+      if (!user.token || user.role !== 'admin') {
+        throw new GraphQLError('Not authorized');
+      }
+      const deletedCat: Cat = (await catModel.findByIdAndDelete(cat.id)) as Cat;
+      return deletedCat;
+    },
+  },
+};
+
+export default catResolver;
--- src/api/resolvers/userResolver.ts
+++ src/api/resolvers/userResolver.ts
@@ -2,6 +2,164 @@ import {GraphQLError} from 'graphql';
 import {Cat} from '../../interfaces/Cat';
 import LoginMessageResponse from '../../interfaces/LoginMessageResponse';
 import {User, UserIdWithToken} from '../../interfaces/User';
+import dotenv from 'dotenv';
+dotenv.config();
 // TODO: create resolvers based on user.graphql
 // note: when updating or deleting a user don't send id to the auth server, it will get it from the token
 // note2: when updating or deleting a user as admin, you need to check if the user is an admin by checking the role from the user object
+
+const userResolver = {
+  Cat: {
+    owner: async (cat: Cat) => {
+      const response = await fetch(
+        `${process.env.AUTH_URL}/users/${cat.owner}`
+      );
+      if (!response.ok) {
+        throw new GraphQLError('Not authorized');
+      }
+      const user = (await response.json()) as User;
+      return user;
+    },
+  },
+  Query: {
+    users: async () => {
+      const response = await fetch(`${process.env.AUTH_URL}/users`);
+      if (!response.ok) {
+        throw new GraphQLError('Not authorized');
+      }
+      const users = (await response.json()) as User[];
+      return users;
+    },
+    userById: async (_: unknown, user: {id: string}) => {
+      const response = await fetch(`${process.env.AUTH_URL}/users/${user.id}`);
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText);
+      }
+      const userById = (await response.json()) as User;
+      return userById;
+    },
+    checkToken: async (_: unknown, __: unknown, user: UserIdWithToken) => {
+      const response = await fetch(`${process.env.AUTH_URL}/users/token`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText);
+      }
+      const userFromToken = (await response.json()) as User;
+      return userFromToken;
+    },
+  },
+  Mutation: {
+    login: async (
+      _: unknown,
+      user: {credentials: {username: string; password: string}}
+    ) => {
+      const response = await fetch(`${process.env.AUTH_URL}/auth/login`, {
+        method: 'POST',
+        body: JSON.stringify(user.credentials),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText);
+      }
+      const loginResponse = (await response.json()) as LoginMessageResponse;
+      return loginResponse;
+    },
+    register: async (_: unknown, user: {user: User}) => {
+      const response = await fetch(`${process.env.AUTH_URL}/users`, {
+        method: 'POST',
+        body: JSON.stringify(user.user),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText);
+      }
+      const registerResponse = (await response.json()) as LoginMessageResponse;
+      return registerResponse;
+    },
+    updateUser: async (
+      _: unknown,
+      user: User,
+      userWithToken: UserIdWithToken
+    ) => {
+      const response = await fetch(`${process.env.AUTH_URL}/users`, {
+        method: 'PUT',
+        body: JSON.stringify(user),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userWithToken.token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText);
+      }
+      const updateUser = (await response.json()) as User;
+      return updateUser;
+    },
+    updateUserAsAdmin: async (
+      _: unknown,
+      user: User,
+      userWithToken: UserIdWithToken
+    ) => {
+      if (!userWithToken.token || !userWithToken.role.includes('admin'))
+        return null;
+      const response = await fetch(`${process.env.AUTH_URL}/users/${user.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(user),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userWithToken.token}`,
+          role: userWithToken.role,
+        },
+      });
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText);
+      }
+      const updateUser = (await response.json()) as User;
+      return updateUser;
+    },
+    deleteUser: async (_: unknown, __: unknown, user: UserIdWithToken) => {
+      const response = await fetch(`${process.env.AUTH_URL}/users`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText);
+      }
+      const deleteUser = (await response.json()) as User;
+      return deleteUser;
+    },
+    deleteUserAsAdmin: async (
+      _: unknown,
+      user: User,
+      userWithToken: UserIdWithToken
+    ) => {
+      if (!userWithToken.token || !userWithToken.role.includes('admin'))
+        return null;
+      const response = await fetch(`${process.env.AUTH_URL}/users/${user.id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userWithToken.token}`,
+          role: userWithToken.role,
+        },
+      });
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText);
+      }
+      const deleteUser = (await response.json()) as User;
+      return deleteUser;
+    },
+  },
+};
+
+export default userResolver;
